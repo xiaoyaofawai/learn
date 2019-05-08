b7c963eb4c9c36b3c6ab6c4fe7d6d479e847c09d
Wangbixiao <wbx19920722>
2019-05-08 23:20:18 +0800

    添加每天备份脚本

---
+++ redis/redis_rdb_copy_daily.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cur_date=`date +%Y%m%d`
+rm -rf /usr/local/redis-5.0.4/snapshotting/$cur_date
+mkdir -p /usr/local/redis-5.0.4/snapshotting/$cur_date
+cp /var/redis/6379/dump.rdb /usr/local/redis-5.0.4/snapshotting/$cur_date
+
+del_date=`date -d -1month +%Y%m%d`
+rm -rf /usr/local/redis-5.0.4/snapshotting/$del_date
